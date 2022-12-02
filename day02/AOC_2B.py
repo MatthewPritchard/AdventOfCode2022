e8adec2f50bc1fbaf88a94cbe763ea8c3b0f3e33
Matthew Pritchard <matthew.pritchard@booking.com>
2022-12-02 10:56:55 +0000

    reformatting

--- day02/AOC_2B.py
+++ day02/AOC_2B.py
@@ -4,7 +4,7 @@ with open("AOC_2_input.txt") as file:
         theirs, result = line.strip().split(" ")
         if result == "X":
             totalScore += 0  # redundant
-            totalScore += (((ord(theirs) - 65) + 2) % 3)+1
+            totalScore += (((ord(theirs) - 65) + 2) % 3) + 1
         elif result == "Y":
             totalScore += 3
             totalScore += ord(theirs) - 64

6d0296550b9912f01d9eed87dc758b9692856477
Matthew Pritchard <matthew.pritchard@booking.com>
2022-12-04 12:37:27 +0000

    day 4

---
+++ day04/AOC_4A.py
@@ -0,0 +1,7 @@
+with open("input.txt") as f:
+    count = 0
+    for line in f:
+        (a, b), (c, d) = ((int(j) for j in i.split("-")) for i in line.strip().split(","))
+        if (a in range(c, d + 1) and b in range(c, d + 1)) or (c in range(a, b + 1) and d in range(a, b + 1)):
+            count += 1
+    print(count)
